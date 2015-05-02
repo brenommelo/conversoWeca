c4a4b1ddeb76fb38ca07a684d7c1d7ef4aecea74
brenommelo <brenommelo@gmail.com>
2015-05-02 12:25:13 -0300

    classe weka

---
+++ conversoWeca/ConversorBdWeka/src/conversorbdweka/weka/weka.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package conversorbdweka.weka;
+
+/**
+ *
+ * @author breno
+ */
+public class weka {
+    private String tempoLaudo;
+    private String glasgowResult;
+    private String minessotaResult;
+    private String PRESSAOARTERIAL;
+    private String ALTURA;
+    private String PESO;
+    private String IMC;
+    private String ESTADOCIVIL;
+    private String ESCOLARIDADE;
+    private String RENDA;
+    private String ENCAMINHARPACIENTE;
+    private String DIURETICOS;
+    private String DIGITAL;
+    private String BETABLOQUEADORES;
+    private String INIBIDORESDAENZIMACONVERSORA;
+    private String AMIODARONA;
+    private String BLOQUEADORESDECALCIO;
+    private String OUTROS;
+    private String NENHUM;
+    private String HIPERTENCAOARTERIAL;
+    private String DOENCADECHAGAS;
+    private String OBESIDADE;
+    private String DIABETESMELLITUS;
+    private String TABAGISMO;
+    private String REVASCULARIZACAMIOCARDICAPREVIA;
+    private String HISTORICOFAMILIARDOENCACORONARIANA;
+    private String INFARTOMIOCARDIOPREVIO;
+    private String DISLIPIDEMIA;
+    private String DOENCARENALCRONICA;
+    private String DOENCAPULMONARCRONICA;
+
+    private String DATAULTIMOEPISODIODor;
+
+    private String DURACAOULTIMOEPISODIOdor;
+
+    private String HAQUANTOTEMPOAPRESENTAVASINTOMASdor;
+    private String MEMBROSUPERIORDIREITOdor;
+
+    private String MEMBROSUPERIORESQUERDOdor;
+
+    private String PESCOCOdor;
+
+    private String DORSOdor;
+
+    private String REGIAOPRECORDIALdor;
+
+    private String REGIAOTORACICADIREITAdor;
+
+    private String EPIGASTRIOdor;
+
+    private String DISPNEIAassociadaAdor;
+
+    private String SUDORESEassociadaAdor;
+
+    private String NAUSEASEOUVOMITOSassociadaAdor;
+
+    private String TONTEIRAassociadaAdor;
+
+    private String SINCOPEassociadaAdor;
+
+    private String PALPITACAOassociadaAdor;
+    private String CLASSIFICACAODADOR;
+    private String nvlDor;
+    private String PROVOCADAPORESFORCOEOUEMOCAOdor;
+
+    private String ALIVIADAPORREPOUSOOUNITRATOdor;
+
+    private String DESCONFORTORETROESTERNALdor;
+
+}
